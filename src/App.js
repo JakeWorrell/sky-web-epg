71209d59f06f0f04055c237d2e4b74ca806d87f8
Jake Worrell <jake.worrell@chalkstring.com>
2020-09-12 23:20:23 +0100

    introduce some real routes between pages, a 'not found' route, links back, and a way to specify a hard coded link on MenuItem

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Link} from "react-router-dom";
 import slugify from "slugify";
+import Switch from "react-router-dom/es/Switch";
+import Route from "react-router-dom/es/Route";
 
 class MenuItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      to: props.to ?? slugify(this.props.children.toLowerCase()),
     };
   }
 
   render() {
     return (
-        <li><div class="num">{this.props.number}</div><Link to={slugify(this.props.children.toLowerCase())}>{this.props.children.toUpperCase()}</Link></li>
+        <li><div class="num">{this.props.number}</div><Link to={this.state.to}>{this.props.children.toUpperCase()}</Link></li>
     );
   }
 }
@@ -22,26 +24,43 @@ class MenuItem extends React.Component {
 function App() {
   return (
     <BrowserRouter>
-      <section className="menulisting" id="main">
-          <MenuItem number="1">All Channels</MenuItem>
-          <MenuItem number="2">Entertainment</MenuItem>
-          <MenuItem number="3">Lifestyle & Culture</MenuItem>
-          <MenuItem number="4">Movies</MenuItem>
-          <MenuItem number="5">Sports</MenuItem>
-          <MenuItem number="6">News</MenuItem>
-          <MenuItem number="7">Documentaries</MenuItem>
-          <MenuItem number="8">Kids</MenuItem>
-          <MenuItem number="9">Music</MenuItem>
-          <MenuItem number="0">More...</MenuItem>
-
-        <div className="links">
-          <a onClick="toPage('main', 'boxoffice')">Go Right</a>
-          <a onClick="toPage('main', 'planner')">Planner</a>
-          <a onClick="toPage('main', 'az')">A-Z</a>
-        </div>
-      </section>
+      <Switch>
+        <Route exact path="/" component={MainMenu} />
+        <Route exact path="/all-channels" component={AllChannels} />
+        <Route exact path='*' component={DeadLink} />
+      </Switch>
     </BrowserRouter>
   );
 }
 
+function MainMenu() {
+    return (
+        <section className="menulisting" id="main">
+            <MenuItem number="1">All Channels</MenuItem>
+            <MenuItem number="2">Entertainment</MenuItem>
+            <MenuItem number="3">Lifestyle & Culture</MenuItem>
+            <MenuItem number="4">Movies</MenuItem>
+            <MenuItem number="5">Sports</MenuItem>
+            <MenuItem number="6">News</MenuItem>
+            <MenuItem number="7">Documentaries</MenuItem>
+            <MenuItem number="8">Kids</MenuItem>
+            <MenuItem number="9">Music</MenuItem>
+            <MenuItem number="0">More...</MenuItem>
+        </section>
+    )
+}
+
+function AllChannels() {
+    return (
+        <section className="menulisting" id="main">
+            <MenuItem number="1" to={"/"}>This means it works</MenuItem>
+        </section>
+    )
+}
+
+function DeadLink() {
+    return (
+        <div>This page doesn't exist. <Link to={'/'}>go back</Link></div>
+    )
+}
 export default App;
